4a35389ed8b1a334b7863fdadb653942ee15b8e5
tome <tomex.beta@gmail.com>
2016-01-23 21:23:30 +0900

    入力の時のタイプ訳で処理することにした。

--- makeLearingFile/makeLearingFile/Program.cs
+++ makeLearingFile/makeLearingFile/Program.cs
@@ -34,7 +34,9 @@ namespace makeLearingFile
             public void exec()
             {
                 //ファイルリストの読み込み
-                ReadFileListTest(FaceImageManage.FACE_TYPE.FUJIKIN);
+//                ReadFileListTest(FACE_TYPE.FUJIKIN);
+                //ファイルリストの読み込み
+                ReadFileListTest(FACE_TYPE.KUBOTA);
 
                 //顔部分の切り出し
                 ExtractFace();
@@ -47,10 +49,11 @@ namespace makeLearingFile
                 CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile(@"C:\opencv2.4.8\sources\data\haarcascades\haarcascade_frontalface_alt.xml");
                 CvMemStorage strage = new CvMemStorage(0);   // メモリを確保
 
+                //フジキンリストの処理
                 int read_count = 0;
-                while( read_count < this.FujikinFileList.Count())
+                while( read_count < this.FaceFileList.Count())
                 {
-                    string input_file_path = this.FujikinFileList[read_count];
+                    string input_file_path = this.FaceFileList[read_count];
 
                     using (IplImage img = new IplImage(input_file_path))
                     {
@@ -58,32 +61,19 @@ namespace makeLearingFile
                         IplImage gray_image = Cv.CreateImage(new CvSize(img.Width,img.Height),BitDepth.U8,1);
                         Cv.CvtColor(img, gray_image, ColorConversion.BgrToGray);
 
-                        //発見した矩形を描画する
+                        //発見した矩形
                         var result = Cv.HaarDetectObjects(gray_image,cascade, strage);
                         for (int i = 0; i < result.Total; i++)
                         {
+                            //矩形の大きさに書き出す
                             CvRect rect = result[i].Value.Rect;
-
                             Cv.Rectangle(img, rect, new CvColor(255, 0, 0));
 
                             //矩形部分をファイル出力する
                             img.ROI = rect;
-
-//                            imwrite();
-//                            Cv.Mat
-
-                            using (new CvWindow(img))
-                            {
-                                Cv.WaitKey();
-                            }                           
-//　                          imwrite("output.png",cut_img);
+                            string out_name = @"out\out" + read_count + @"_"+ i + @".bmp";
+                            Cv.SaveImage(out_name, img);
                         }
-
-
-
-
-
-
                     }
                     read_count++;
                 }
@@ -93,23 +83,31 @@ namespace makeLearingFile
             //顔写真リストファイルを読み込み
             private void ReadFileListTest(FACE_TYPE f_type)
             {
+                string read_list = @"";
+
                 if (f_type == FACE_TYPE.FUJIKIN)
                 {
-                    //リストファイルと読みこんでファイル名をとる
-                    using (StreamReader sr = new StreamReader(@"I:\myprog\github\kubokinJudge\data\fujikin\fujikin_list.txt"))
+                    read_list = @"I:\myprog\github\kubokinJudge\data\fujikin\fujikin_list.txt";
+                }
+                else if (f_type == FACE_TYPE.KUBOTA)
+                {
+                    read_list = @"I:\myprog\github\kubokinJudge\data\kubota\kubota_list.txt";
+                }
+
+                //リストファイルと読みこんでファイル名をとる
+                using (StreamReader sr = new StreamReader(read_list))
+                {
+                    //1行づつ読み込む
+                    while (sr.Peek() > -1)
                     {
-                        //1行づつ読み込む
-                        while (sr.Peek() > -1)
-                        {
-                            this.FujikinFileList.Add(sr.ReadLine());
-                        }
-                        //閉じる
-                        sr.Close();
+                        FaceFileList.Add(sr.ReadLine());
                     }
+                    //閉じる
+                    sr.Close();
                 }
             }
 
-            List<string> FujikinFileList = new List<string>();
+            List<string> FaceFileList = new List<string>();
         }
     }
 }
