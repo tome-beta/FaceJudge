dbf8675ae52711ef703127d1fb723df62ece016f
tome <tomex.beta@gmail.com>
2016-01-23 15:07:28 +0900

    学習ファイル作成ツールを追加

---
+++ makeLearingFile/makeLearingFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace makeLearingFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //読み込んだファイルから顔認識を行ってファイルとして切り出す
+
+
+
+
+        }
+    }
+}
