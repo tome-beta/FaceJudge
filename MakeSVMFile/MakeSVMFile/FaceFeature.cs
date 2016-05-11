40f6a1f88b871772e6addfb7376e01b4fce2ba77
tome <tomex.beta@gmail.com>
2016-05-11 23:20:06 +0900

    #31 鼻と口の検索位置を制限して検出ミスを減らした

--- MakeSVMFile/MakeSVMFile/FaceFeature.cs
+++ MakeSVMFile/MakeSVMFile/FaceFeature.cs
@@ -97,20 +97,31 @@ namespace MakeSVMFile
 
                 //発見した矩形
                 this.EyeResult = Cv.HaarDetectObjects(gray_image, eye_cascade, strage);
-                this.NoseResult = Cv.HaarDetectObjects(gray_image, nose_cascade, strage);
+
+                //鼻は画像の真ん中の方だけ
+                {
+                    IplImage gray_nose_image = Cv.CreateImage(new CvSize(tmp_image.Width, tmp_image.Height), BitDepth.U8, 1);
+                    Cv.CvtColor(tmp_image, gray_nose_image, ColorConversion.BgrToGray);
+                    CvRect rect = new CvRect(0, (int)(tmp_image.Height*0.25), tmp_image.Width, tmp_image.Height / 2);
+                    gray_nose_image.ROI = rect;
+//                  new CvWindow(gray_nose_image);
+//                  Cv.WaitKey();
+
+                    this.NoseResult = Cv.HaarDetectObjects(gray_nose_image, nose_cascade, strage);
+                }
 
                 //口は画像の下半分だけを調べる
-                IplImage gray_mouth_image = Cv.CreateImage(new CvSize(tmp_image.Width, tmp_image.Height), BitDepth.U8, 1);
-                Cv.CvtColor(tmp_image, gray_mouth_image, ColorConversion.BgrToGray);
-                CvRect rect = new CvRect(0, tmp_image.Height / 2, tmp_image.Width, tmp_image.Height / 2);
-                gray_mouth_image.ROI = rect;
-                new CvWindow(gray_mouth_image);
-                 Cv.WaitKey();
-
-                this.MouthResult = Cv.HaarDetectObjects(gray_mouth_image, mouth_cascade, strage);
-
-                    //初期化
-                    DataInit();
+                {
+                    IplImage gray_mouth_image = Cv.CreateImage(new CvSize(tmp_image.Width, tmp_image.Height), BitDepth.U8, 1);
+                    Cv.CvtColor(tmp_image, gray_mouth_image, ColorConversion.BgrToGray);
+                    CvRect rect = new CvRect(0, (int)(tmp_image.Height *0.66), tmp_image.Width, tmp_image.Height / 3);
+                    gray_mouth_image.ROI = rect;
+                    new CvWindow(gray_mouth_image);
+                     Cv.WaitKey();
+                    this.MouthResult = Cv.HaarDetectObjects(gray_mouth_image, mouth_cascade, strage);
+                }
+                //初期化
+                DataInit();
                 //デバッグ用の表示
                 DebugPrint(tmp_image, this.ReadCount);
 
@@ -296,6 +307,8 @@ namespace MakeSVMFile
             for (int i = 0; i < this.NoseResult.Total; i++)
             {
                 CvRect rect = this.NoseResult[i].Value.Rect;
+                //中央だけ検索しているので座標を加える
+                rect.Y += (int)(img.Height * 0.25);
                 int rect_size = rect.Height * rect.Width;
 
                 //画像の中央に位置するはず
@@ -317,7 +330,7 @@ namespace MakeSVMFile
             {
                 CvRect rect = this.MouthResult[i].Value.Rect;
                 //下半分だけ検索しているのでその座標が出ているから加える
-                rect.Y += (img.Height / 2);
+                rect.Y += (int)(img.Height *0.66);
                 int rect_size = rect.Height * rect.Width;
 
                 //画像の下半分にあるはず
@@ -393,6 +406,8 @@ namespace MakeSVMFile
             {
                 //矩形の大きさに書き出す
                 CvRect rect = NoseResult[i].Value.Rect;
+                //中央だけ検索しているので座標を加える
+                rect.Y += (int)(img.Height * 0.25);
                 Cv.Rectangle(img, rect, new CvColor(0, 255, 0));
             }
 
@@ -402,7 +417,7 @@ namespace MakeSVMFile
                 //矩形の大きさに書き出す
                 CvRect rect = MouthResult[i].Value.Rect;
                 //下半分だけ検索しているのでその座標が出ているから加える
-                rect.Y += (img.Height / 2);
+                rect.Y += (int)(img.Height * 0.66);
                 Cv.Rectangle(img, rect, new CvColor(0, 0, 255));
             }
 
